c12376dc562fed8f1904811343eb68eb7a738589
20211tiimi0338 <dudinha140405@gmail.com>
2022-09-02 09:46:12 -0300

    Fiz o Passo 7 do Docs

--- app/src/main/java/fernandes/maria/galeria/MainActivity.java
+++ app/src/main/java/fernandes/maria/galeria/MainActivity.java
@@ -1,7 +1,13 @@
 package fernandes.maria.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Seta o elemento de interface tbMain como a ActionBar da tela da MainActivity
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+
     }
+
+    //Método que cria um inflador de menu, criando opções de menu disponíveis em um arquivo de menu
+    //anteriormente criado (main_activity_tb.xml) e as adiciona na ActionBar de MainAcitivity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+    //Método que executa determinado código toda vez que um item da ToolBar é selecionado
+    //Quando o ícone de camêra é selecionado, executa um código que dispara a camêra do celular
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opCamera:
+                dispatchTakePictureIntent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/fernandes/maria/galeria/PhotoActivity.java
+++ app/src/main/java/fernandes/maria/galeria/PhotoActivity.java
@@ -1,8 +1,14 @@
 package fernandes.maria.galeria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        //Seta o elemento de interface tbPhoto como a ActionBar da tela da PhotoActivity
+
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+
+        //Obtém de PhotoActivity a ActionBar e habilita um botão de "Voltar" nela
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
+
+    //Método que cria um inflador de menu, criando opções de menu disponíveis em um arquivo de menu
+    // anteriormente criado (photo_activity_tb.xml) e as adiciona na ActionBar de PhotoActivity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
+
+    //Método que executa determinado código toda vez que um item da ToolBar é selecionado
+    //Quando o ícone de compartilhamento é selecionado, executa um código que compartilha a foto
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
